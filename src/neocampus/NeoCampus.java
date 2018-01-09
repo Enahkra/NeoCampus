83a524f1d57684001762fd16c8b118ae05ac9d35
Samuel <sweetarkhane@live.fr>
2018-01-09 22:19:06 +0100

    Tentative de mise en page

--- src/neocampus/NeoCampus.java
+++ src/neocampus/NeoCampus.java
@@ -14,9 +14,6 @@ public class NeoCampus {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        /dzzzzzzzzzzzzzzzzzzzzz;
-    }
+    
     
 }
