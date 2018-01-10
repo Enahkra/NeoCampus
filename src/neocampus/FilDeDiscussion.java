0821ae52bd8b5dc5569e5cdc22dff565a6de2bf9
moussabrahim <moussabrahim@moussabrahim-HP-Notebook>
2018-01-10 22:54:08 +0100

    classe

---
+++ src/neocampus/FilDeDiscussion.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package neocampus;
+
+/**
+ *
+ * @author moussabrahim
+ */
+public class FilDeDiscussion {
+    
+    private int idFilDeDiscussion;
+    private String titre ;
+  // ArrayList <Message> list ;
+    
+    
+}
---
+++ src/neocampus/Serveur.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package neocampus;
+
+/**
+ *
+ * @author moussabrahim
+ */
+public interface Serveur {
+    public void ajouerUtilisateur();
+    public void modifierUtilisateur();
+    public void supprimerUtilisateur ();
+    public void ajouterGroupe();
+    public void modifierGroupe();
+    public void supprimerGroupe();
+    public void gererAdhesion();
+    
+    
+    
+}
