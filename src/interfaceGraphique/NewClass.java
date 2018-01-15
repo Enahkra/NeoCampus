4b8568b48829f9e92a3f545ddf1ae5f58d60f7c8
moussabrahim <moussabrahim@moussabrahim-HP-Notebook>
2018-01-15 21:35:48 +0100

    classe

---
+++ src/interfaceGraphique/NewClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaceGraphique;
+
+/**
+ *
+ * @author moussabrahim
+ */
+public class NewClass extends javax.swing.JFrame {
+    
+    public NewClass (){
+        setSize (400,400);
+        setLocation(300,600);
+        setVisible(true);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }
+    
+}
