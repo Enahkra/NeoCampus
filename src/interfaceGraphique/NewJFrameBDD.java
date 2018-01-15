aff2a92edbcbe689e0e7c19be12b97e1d6b34760
moussabrahim <moussabrahim@moussabrahim-HP-Notebook>
2018-01-15 22:53:49 +0100

    classe

--- src/interfaceGraphique/NewJFrameBDD.java
+++ src/interfaceGraphique/NewJFrameBDD.java
@@ -5,6 +5,7 @@
  */
 package interfaceGraphique;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
@@ -23,6 +24,38 @@ public class NewJFrameBDD extends javax.swing.JFrame {
      */
     public NewJFrameBDD() {
         initComponents();
+    }
+    
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Jclass.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Jclass.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Jclass.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Jclass.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new NewJFrameBDD().setVisible(true);
+            }
+        });
     }
 
     /**
@@ -138,11 +171,13 @@ public class NewJFrameBDD extends javax.swing.JFrame {
 
     private void button1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_button1ActionPerformed
       button1.addActionListener(new ActionListener () {
-           public void actionPerformed (){
-               NewClass nc = new NewClass();
+           public void actionPerformed (ActionEvent e){
+              Jclass nc = new Jclass();
+              nc.setVisible(true);
                
            }
-       });
+      });
+          
        
     }//GEN-LAST:event_button1ActionPerformed
 
